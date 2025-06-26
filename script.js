82d9a7877766903365d719185fb9a7e6802c0947
Aniket Tikudave <anikettikudave187@gmail.com>
2025-06-26 14:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares=document.querySelectorAll('.square');
+
+squares.forEach((square, index)=>{
+	square.addEventListener("mouseenter",()=>{
+		squares.forEach((otherSquare)=>{
+			if(otherSquare!==square){
+				otherSquare.style.backgroundColor='#6F4E37';
+			}
+		});
+	});
+
+	square.addEventListener("mouseleave",()=>{
+		squares.forEach((s)=>{
+			s.style.backgroundColor='#E6E6FA';
+		});
+	});
+});
